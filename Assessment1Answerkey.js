b1821726524bafed543ab1842d433b1e9747e67f
Kazaguya <54612164+Kazaguya@users.noreply.github.com>
2022-01-14 16:26:23 -0600

    Update Assessment1Answerkey.js

--- Assessment1Answerkey.js
+++ Assessment1Answerkey.js
@@ -2,20 +2,20 @@
 "(hint, delcaring a variable is just naming a variable you plan to use later)"
 
 //Your answer goes here
-
+let EriqRulez;
 //
 
 "Write an example of how you would initialize a variable (e.g. setting EriqRulez equal to true, assuming that EriqRulez has already been declared)"
 "(hint, initializing a variable that has already been named is just giving it a value)"
 
 //Your answer goes here
-
+EriqRulez = true;
 //
 
 "Write an example of how you would print out 'Eriq be dope'"
 
 //your answer goes here
-
+console.log('Eriq be dope');
 //
 
 "Write what each of the operators do in JavaScript"
@@ -23,42 +23,50 @@
 
 "  Opr  |  Use  "
 "-------+-------"
-"  =    |       "
-"  ==   |       "
-"  ===  |       "
-"  >    |       "
-"  <    |       "
-"  >=   |       "
-"  <=   |       "
-"  !    |       "
-"  ||   |       "
-"  &&   |       "
-"  +    |       "
-"  -    |       "
-"  /    |       "
-"  *    |       "
-"  ++   |       "
-"  --   |       "
-"  ;    |        <---(hint, this one isn't actually an operator, but it is really important to know what it does)"
+"  =    |  initializing a variable     "
+"  ==   |  checking equality     "
+"  ===  |  really checking equality     "
+"  >    |  greater than     "
+"  <    |  less than     "
+"  >=   |  greater than or equals to     "
+"  <=   |  less than or equals to     "
+"  !    |  not     "
+"  ||   |  or     "
+"  &&   |  and     "
+"  +    |  plus     "
+"  -    |  minus     "
+"  /    |  divide     "
+"  *    |  multiply     "
+"  ++   |  increment by 1     "
+"  --   |  decrement by 1     "
+"  ;    |  ends a line of code      <---(hint, this one isn't actually an operator, but it is really important to know what it does)"
  
  
 "Write a conditional statement (also known as if/else) that prints out 'Eriq is awesome' if the variable 'x' is greater than 0"
 
 //your answer goes here
-
+if (x > 0) {
+  console.log('Eriq is awesome');
+}
 //
 
 "Write a conditional statement (also known as if/else) that prints out 'Damn, Eriq is so cool' if the variable 'x' is in between 0 and 10 (inclusive)"
 "(hint, saying a number is in between 0 and 10 is the same as saying it is greater than or equal to 0 and less than or equal to 10)"
 
 //your answer goes here
-
+if (x >= 0 && x <= 10) {
+  console.log('Damn, Eriq is so cool');
+}
 //
 
 "Write a conditional statement (also known as if/else) that prints out 'Eriq has drip' if the variable 'x' is greater than 5, and prints out 'Eriq has minor drip' otherwise"
 
 //your answer goes here
-
+if (x > 5) {
+  console.log('Eriq has drip');
+} else {
+  console.log('Eriq has minor drip');
+}
 //
 
 
