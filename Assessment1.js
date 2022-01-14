a715e85e1052a09802c744452bdbcee984ba79ff
Kazaguya <54612164+Kazaguya@users.noreply.github.com>
2022-01-14 15:04:41 -0600

    Update and rename Assessment#1.js to Assessment1.js

---
+++ Assessment1.js
@@ -0,0 +1,67 @@
+"Write an example of how you would declare a variable (e.g. declaring a variable named EriqRulez)"
+"(hint, delcaring a variable is just naming a variable you plan to use later)"
+
+//Your answer goes here
+
+//
+
+"Write an example of how you would initialize a variable (e.g. setting EriqRulez equal to true, assuming that EriqRulez has already been declared)"
+"(hint, initializing a variable that has already been named is just giving it a value)"
+
+//Your answer goes here
+
+//
+
+"Write an example of how you would print out 'Eriq be dope'"
+
+//your answer goes here
+
+//
+
+"Write what each of the operators do in JavaScript"
+"(hint, an operator is a mini instruction)"
+
+"Opr" | "Use"
+------+------
+=     |
+==    |
+===   |
+>     |
+<     |
+>=    |
+<=    |
+!     |
+||    |
+&&    |
++     |
+-     |
+/     |
+*     |
+++    |
+--    |
+;     |
+ 
+ 
+"Write a conditional statement (also known as if/else) that prints out 'Eriq is awesome' if the variable 'x' is greater than 0"
+
+//your answer goes here
+
+//
+
+"Write a conditional statement (also known as if/else) that prints out 'Damn, Eriq is so cool' if the variable 'x' is in between 0 and 10 (inclusive)"
+"(hint, saying a number is in between 0 and 10 is the same as saying it is greater than or equal to 0 and less than or equal to 10)"
+
+//your answer goes here
+
+//
+
+"Write a conditional statement (also known as if/else) that prints out 'Eriq has drip' if the variable 'x' is greater than 5, and prints out 'Eriq has minor drip' otherwise"
+
+//your answer goes here
+
+//
+
+
+
+
+
